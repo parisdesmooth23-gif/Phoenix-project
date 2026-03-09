8118ff0e015e761af3305e70bb3fb70137903839
parisdesmooth23-gif <parisdesmooth23@gmail.com>
2026-03-09 12:00:09 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,6 @@
+const gameText= document.getElementByld('gme-text2');
+const userInput = document.getElementByld('user-input2');
+const submitBtn = document.getElementByld('submit-btn2')
+funtion print(text){
+  gameText.innerText +=text + "/n";
+  gameText.scrollTop +gameText.scrollHeight;}
